5fd84e4423718ebc575a6043e119ad89edd3be25
soufian elmsiyah <soufian.elmsiyah@gmail.com>
2020-10-16 08:55:57 +0100

    Test Endpoints

---
+++ WebAPITest/MachinesControllerTest.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Repository.Data;
+using Service.Interfaces;
+using Service.Services;
+using WebAPI;
+using WebAPI.Controllers;
+using Xunit;
+
+namespace WebAPITest
+{
+    [Collection("Integration Tests")]
+    public class MachinesControllerTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public MachinesControllerTest(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+         public async Task Get_Machines_ReturnsSuccessStatusCode()
+         {
+       
+            var response = await _httpClient.GetAsync("/api/machines");
+
+            response.EnsureSuccessStatusCode();
+         }
+
+        [Fact]
+        public async Task Get_Machines_ReturnsContent()
+        {
+            var response = await _httpClient.GetAsync("/api/machines");
+
+            Assert.NotNull(response.Content);
+            Assert.True(response.Content.Headers.ContentLength > 0); 
+        }
+
+        [Fact]
+        public async Task Get_Machine_ReturnsSuccessStatusCode()
+        {
+
+            var response = await _httpClient.GetAsync("/api/machines/machine/1");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Get_Machine_ReturnsContent()
+        {
+            var response = await _httpClient.GetAsync("/api/machines/machine/1");
+
+            Assert.NotNull(response.Content);
+            Assert.True(response.Content.Headers.ContentLength > 0);
+        }
+
+        [Fact]
+        public async Task Get_TotalProduction_ReturnsSuccessStatusCode()
+        {
+            var response = await _httpClient.GetAsync("/api/machines/machine/totalproduction/1");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Get_TotalProduction_ReturnsContent()
+        {
+            var response = await _httpClient.GetAsync("/api/machines/machine/totalproduction/1");
+
+            Assert.NotNull(response.Content);
+            Assert.True(response.Content.Headers.ContentLength > 0);
+        }
+
+
+        [Fact]
+        public async Task Delete_Machine_ReturnsSuccessStatusCode()
+        {
+            var response = await _httpClient.DeleteAsync("/api/machines/machine/1");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Delete_Machine_ReturnsContent()
+        {
+            var response = await _httpClient.DeleteAsync("/api/machines/machine/1");
+
+            Assert.NotNull(response.Content);
+            Assert.True(response.Content.Headers.ContentLength > 0);
+        }
+    }
+}
